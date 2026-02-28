3a8c62b8a458d1ade3b36de97efe572042c42cec
cab <>
2026-02-28 16:25:21 +0700

    add  commandservice and deploy on k8s

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddAutoMapper(cfg => cfg.AddMaps(AppDomain.CurrentDomain.GetAssemblies()));
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+Console.WriteLine($"--> CommandService Endpoint: {builder.Configuration["CommandService"]}");
 
 var app = builder.Build();
 
